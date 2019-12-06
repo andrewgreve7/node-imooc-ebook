7522aa564ed1ffecf90e2cab818698c797b15b4c
andrew <andrew.greve7@yahoo.com>
2019-12-06 10:51:11 +0800

    nodejd

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express=require('express')
-const app=express()
-app.get('/',(req,res)=>{
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
     res.send(new Date().toDateString())
 })
 
-const server=app.listen(3000,()=>{
-    const host=server.address().address
-    const port=server.address().port
+const server = app.listen(3000, () => {
+    const host = server.address().address
+    const port = server.address().port
 
-    console.log('server is listening at http://%s:%s',host,port)
+    console.log('server is listening at http://%s:%s', host, port)
 })
